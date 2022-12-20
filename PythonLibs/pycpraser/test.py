8c748a2f3a2e29a5ce98cce203d0af1585aa5b42
h13 <1491918308@qq.com>
2022-12-20 19:44:56 +0800

    vault backup: 2022-12-20 19:44:56

--- PythonLibs/pycpraser/test.py
+++ PythonLibs/pycpraser/test.py
@@ -8,7 +8,9 @@ char test(char a)
 """
 #_Alignas(char) const volatile static _Thread_local int a;
 code = """
-int func(void);
+typedef struct _lv_draw_ctx_t  {};
+typedef struct {} lv_draw_rect_dsc_t;
+void safe_lv_draw_polygon(struct _lv_draw_ctx_t * draw_ctx, const lv_draw_rect_dsc_t * draw_dsc, const lv_point_t points, uint16_t point_cnt);
 """
 
 parser = pyc.c_parser.CParser()
