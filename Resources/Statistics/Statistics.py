88c6906169ee62efbd539e14ccbe86abc0b53b62
h13 <1491918308@qq.com>
2025-06-30 16:39:23 +0800

    vault backup: 2025-06-30 16:39:23

--- Resources/Statistics/Statistics.py
+++ Resources/Statistics/Statistics.py
@@ -160,6 +160,7 @@ def plot_repo_stats(daily_line_count, daily_word_count, daily_commit_count,
 def main():
     repo = git.Repo(r"./Notebooks")
     repo.git.pull()
+    repo.git.checkout("master")
     repo_status = []
 
     d = deque(repo.iter_commits())
